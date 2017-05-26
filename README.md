9a6eb0c67fb54b47e5e6d6040e3a1253671c99d3
zyj <jayshzhang@hotmail.com>
2017-05-26 21:32:53 +0800

    信号槽

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
-# QtLearningRoad2
-QT学习之路2的代码
-学习QT
+#QT学习之路2的总结
+
+#4 信号槽
+connect()函数最常用的一般形式:
+
+`connect(sender, signal, receiver, slot);`
+
+### sender:   
+
+发出信号的对象
+
+### signal:  
+
+发送对象发出的信号
+
+### recerver:
+ 
+接收信号的对象
+
+### slot:
+
+接收对象在接收到信号之后所需要的调用的函数
+
--- _2_SignalSlot/main.cpp
+++ _2_SignalSlot/main.cpp
@@ -1,14 +1,21 @@
 #include "mainwindow.h"
 #include <QApplication>
 #include <QPushButton>
+#include <QDebug>
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
+//    QPushButton button("Quit");
+//    QObject::connect(&button, &QPushButton::clicked,
+//                     &QApplication::quit);
+
+    //将一个对象的信号连接到Lambda表达式
     QPushButton button("Quit");
     QObject::connect(&button, &QPushButton::clicked,
-                     &QApplication::quit);
-    button.show();
+                     [](bool){qDebug() << "You clicked me!";
+    });
 
+    button.show();
     return a.exec();
 }
